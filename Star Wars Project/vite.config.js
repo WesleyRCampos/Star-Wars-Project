75c81da6f8e700e45424662ef9445535010c5c6c
wesley de campos rodrigues <wesleyc21rodrigues@gmail.com>
2023-12-11 19:28:30 -0300

    v1.0.2

--- Star Wars Project/vite.config.js
+++ Star Wars Project/vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      fileName: () => `index.js`
+    },
+    rollupOptions: {
+      external: [
+        'react',
+        'react-dom',
+        'react-router-dom',
+        '@reduxjs/toolkit',
+        'react-redux',
+        'prop-types'
+      ]
+    }
+  }
 })
+
