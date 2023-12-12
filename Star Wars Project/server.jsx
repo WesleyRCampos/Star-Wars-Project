89b7037aaae1778358b2aea0fb15e64ddb2a7a40
wesley de campos rodrigues <wesleyc21rodrigues@gmail.com>
2023-12-12 00:02:20 -0300

    sdlfksf

--- Star Wars Project/server.jsx
+++ Star Wars Project/server.jsx
@@ -1,11 +1,18 @@
 const express = require('express')
-
+const { resolve } = require('path')
 const app = express()
 
-app
+app.use('/',
+    express, Static(
+        resolve(
+            __dirname,
+            '/build'
+        )
+    )
+)
 
-.listen(process.env.PORT || 3000. (err) => {
-    if (err) { return console.log(err) }
+    .listen(process.env.PORT || 3000, (err) => {
+        if (err) { return console.log(err) }
 
-    
-})
+        console.log('Alo')
+    })
